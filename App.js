3c2cf236361886f9de7296f5fc8a081790b81cb4
chinhdev <chinhtdph40493@fpr.edu.vn>
2024-04-08 20:35:36 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import App72 from './screens/Bai71'
+
+
+
+const App = () => {
+  return (
+   
+
+  <App72/>
+  )
+}
+
+export default App
+
+ const styles = StyleSheet.create({})
